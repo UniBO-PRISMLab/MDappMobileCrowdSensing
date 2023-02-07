d232cbfbdcc082e3e4910bd65d3b4eecc198cf24
Giacomo <giacomo.galletti2@studio.unibo.it>
2023-02-07 19:41:22 +0100

    Update sourcer_campaign_controller.dart

--- lib/controllers/sourcer_campaign_controller.dart
+++ lib/controllers/sourcer_campaign_controller.dart
@@ -137,7 +137,7 @@ class _SourcerCampaignControllerState extends State<SourcerCampaignController> {
             Flexible(
                 flex: 5,
                 child: Text(
-                  "Sourcing Status:\nuploaded $fileCount files\nchecked $fileChecked of $fileCount\nwhit the contribution of $workersCount workers",
+                  "Sourcing Status:\nuploaded $fileCount files\nchecked $fileChecked of $fileCount\nwith the contribution of $workersCount workers",
                   style: CustomTextStyle.spaceMono(context),
                 )),
             const SizedBox(
